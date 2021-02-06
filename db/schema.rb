0ce36e3c1d3fa8f00401d9027a9bfee18ca4df78
keisukesaito7 <keisukesaito7@gmail.com>
2021-02-06 21:51:18 +0900

    null: falseとpresence: trueを追加（モデルテストのため）

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_02_06_121429) do
 
   create_table "tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text "tweet"
+    t.text "tweet", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
